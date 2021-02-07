94489aad68c61b011e14353b7c5c4c77b1df1e14
Thomas Glaser <thomas@tkglaser.net>
2021-02-07 15:46:28 +0000

    Base debug

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,8 +1,15 @@
-import { Species } from "service";
+import { HelloService, Species } from "service";
 
 export class Api {
   async hello(name: string, species: Species) {
-    const resp = await fetch(`http://localhost:2500/dev/hello?name=${name}&species=${species}`);
+    // test debugging:
+    const svc = new HelloService();
+    const result = svc.hello("debug", Species.Human);
+    console.log(result);
+
+    const resp = await fetch(
+      `http://localhost:2500/dev/hello?name=${name}&species=${species}`
+    );
     return resp.json();
   }
 }
